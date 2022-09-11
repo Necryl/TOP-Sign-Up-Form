37adae22b39ff8f7331f4f8070a8de05169d49ef
Necryl <necrylthewayfarer@gmail.com>
2022-09-11 13:22:47 +0530

    add fake button

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.submitBtn');
+const actualBtn = document.querySelector('#actualBtn');
+
+button.addEventListener('click', event => {
+    if (true) {
+        actualBtn.click();
+    }
+});
